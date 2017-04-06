5eb9269971ac409b9750b1ecf4d935f1e8783155
Sheldon Foster <Sheldon.Foster@coxautoinc.com>
2017-04-06 14:06:12 -0600

    TrackIt screen functionality

--- ProjectTimeTrackerAuth/Controllers/TrackItController.cs
+++ ProjectTimeTrackerAuth/Controllers/TrackItController.cs
@@ -38,22 +38,36 @@ namespace ProjectTimeTrackerAuth.Controllers
             return View();
         }
 
-        // GET: TrackIt/Create
-        public IActionResult Create()
-        {
-            ViewData["ActivityID"] = new SelectList(_context.Activities, "ActivityID", "ActivityID");
-            return View();
-        }
-
         // POST: TrackIt/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TimeLogID,Username,ActivityID,UsersID,StartTime,EndTime")] TimeLog timeLog)
+        public async Task<IActionResult> Create([Bind("ActivityID")] TimeLog timeLog)
         {
             if (ModelState.IsValid)
             {
+                //get last time log with no end time
+                string query = "SELECT TOP 1 * FROM TimeLog WHERE Username = {0} and EndTime is null order by StartTime desc";
+                try {
+                    var endTimeLog = await _context.TimeLogs
+                        .FromSql(query, User.Identity.Name)
+                        .AsNoTracking()
+                        .SingleOrDefaultAsync();
+                    endTimeLog.EndTime = DateTime.Now;
+                    _context.Update(endTimeLog);
+                    await _context.SaveChangesAsync();
+                } catch (DbUpdateConcurrencyException) {
+                    if (!TimeLogExists(timeLog.TimeLogID)) {
+                        return NotFound();
+                    } else {
+                        throw;
+                    }
+                }
+
+
+                timeLog.Username = User.Identity.Name;
+                timeLog.StartTime = DateTime.Now;
                 _context.Add(timeLog);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -62,57 +76,26 @@ namespace ProjectTimeTrackerAuth.Controllers
             return View(timeLog);
         }
 
-        // GET: TrackIt/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var timeLog = await _context.TimeLogs.SingleOrDefaultAsync(m => m.TimeLogID == id);
-            if (timeLog == null)
-            {
-                return NotFound();
-            }
-            ViewData["ActivityID"] = new SelectList(_context.Activities, "ActivityID", "ActivityID", timeLog.ActivityID);
-            return View(timeLog);
-        }
-
-        // POST: TrackIt/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: TrackIt/StopActivity
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TimeLogID,Username,ActivityID,UsersID,StartTime,EndTime")] TimeLog timeLog)
-        {
-            if (id != timeLog.TimeLogID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
+        public async Task<IActionResult> Edit(int id, [Bind("TimeLogID,Username,StartTime,ActivityID")] TimeLog timeLog) {
+            if (ModelState.IsValid) {
+                try {
+                    timeLog.Username = User.Identity.Name;
+                    timeLog.EndTime = DateTime.Now;
                     _context.Update(timeLog);
                     await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TimeLogExists(timeLog.TimeLogID))
-                    {
+                } catch (DbUpdateConcurrencyException) {
+                    if (!TimeLogExists(timeLog.TimeLogID)) {
                         return NotFound();
-                    }
-                    else
-                    {
+                    } else {
                         throw;
                     }
                 }
                 return RedirectToAction("Index");
             }
-            ViewData["ActivityID"] = new SelectList(_context.Activities, "ActivityID", "ActivityID", timeLog.ActivityID);
-            return View(timeLog);
+            return RedirectToAction("Index");
         }
 
         private bool TimeLogExists(int id)
