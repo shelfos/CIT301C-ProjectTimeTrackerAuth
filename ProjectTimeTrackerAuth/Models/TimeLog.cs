2379c786edfb29b509f9ff266b96604663d80f18
Sheldon Foster <Sheldon.Foster@coxautoinc.com>
2017-04-05 20:20:11 -0600

    Add project files.

---
+++ ProjectTimeTrackerAuth/Models/TimeLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTimeTrackerAuth.Models
+{
+    public class TimeLog
+    {
+        public int TimeLogID { get; set; }
+        public string Username { get; set; }
+        public int ActivityID { get; set; }
+        public int UsersID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public Activity Activities { get; set; }
+    }
+}
